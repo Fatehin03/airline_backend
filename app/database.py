d5db794bce23082ad2602604142b40ee32ec4622
Fatehin03 <fatehinalam03@gmail.com>
2026-01-24 19:57:21 +0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg2://postgres:password@localhost:5432/airline_db"
+    "postgresql+psycopg2://airline_db_n8wv_user:91DS9shCFVvtELlfFrsQr78tfiDiPfTR@dpg-d5qctf7pm1nc738ofucg-a:5432/airline_db_n8wv
+"
 )
 
 engine = create_engine(DATABASE_URL, echo=True)
